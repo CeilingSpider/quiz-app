b93a046c0729fc19ea9723f05e330de0b6d4c7b7
Rachel Stulgin <rstulgin@gmail.com>
2019-09-20 17:36:09 -0500

    added functionality to quiz

---
+++ data.js
@@ -0,0 +1,104 @@
+const STORE = [
+    {
+    question: 'What steak alternative does Chris Traeger prepare for Ron and Leslie when they visit him in Indianapolis?',
+    answers: [
+        'Portobello mushrooms',
+        'Black bean burger patties',
+        'tofurkey beer brats',
+        'soy steak'
+    ],
+    correctAnswer: 'Portobello mushrooms',
+},
+{
+    question: 'Jerry and Lil\' Sebastian have this in common:',
+    answers: [
+        'They both have Irritable Bowel Syndrome',
+        'Both of them have astigmatism',
+        'They are Glucatrol buddies',
+        'They both enjoy Paunch Burger'
+    ],
+    correctAnswer: 'They are Glucatrol buddies',
+},
+{
+    question: 'At his inauguration, 18 year-old mayor Ben Wyatt chose this song for the occasion:',
+    answers: [
+        'Ice Ice Baby by Vanilla Ice',
+        'Whoomp! (There It Is) by Tag Team',
+        'This is How We Do It by Montell Jordan',
+        'U Can’t Touch This by MC Hammer'
+    ],
+    correctAnswer: 'Whoomp! (There It Is) by Tag Team',
+},
+{
+    question: 'What is the name of FBI agent Bert Macklin’s brother?',
+    answers: [
+        'Kirk Macklin',
+        'Mac Bertman',
+        'Kurt Macklin',
+        'Kip Hackman'
+    ],
+    correctAnswer: 'Kip Hackman',
+},
+{
+    question: 'Which of these characters has an honorary degree from Notre Dame?',
+    answers: [
+        'Lil\' Sebastian',
+        'Leslie Knope',
+        'Chris Traeger',
+        'Bobby Newport'
+    ],
+    correctAnswer: 'Lil\' Sebastian',
+},
+{
+    question: 'Which of these is NOT one of Jerry\'s names?',
+    answers: [
+        'Terry',
+        'Garry',
+        'Barry',
+        'Harry'
+    ],
+    correctAnswer: 'Harry',
+},
+{
+    question: 'Ron Swanson keeps a sizable amount of _____ in his desk drawer. Remove it or it will begin to smell.',
+    answers: [
+        'Thick-cut bacon',
+        'Steak',
+        'Pork ribs',
+        'Ground chuck'
+    ],
+    correctAnswer: 'Ground chuck',
+},
+{
+    question: 'Jerry is quite talented at _____, though usually nobody notices this.',
+    answers: [
+        'Knitting',
+        'Painting',
+        'Woodcarving',
+        'Sculpting'
+    ],
+    correctAnswer: 'Painting',
+},
+{
+    question: 'Tom Haverford is from:',
+    answers: [
+        'North Carolina',
+        'New Delhi',
+        'Michigan',
+        'South Carolina'
+    ],
+    correctAnswer: 'North Carolina',
+},
+{
+    question: 'Tom creates his own fragrance named:',
+    answers: [
+        'T-Dazzle',
+        'Tommy Fresh',
+        'Allergic',
+        'Tommy Smooth'
+    ],
+    correctAnswer: 'Tommy Fresh',
+}
+
+
+]
---
+++ index.js
@@ -0,0 +1,165 @@
+let questionNumber = 0;
+let score = 0;
+
+//renders HTML for question-answer format
+function generateQuestion() {
+    if (questionNumber < STORE.length) {
+        return `<div class="question-${questionNumber}">
+        <p>${STORE[questionNumber].question}</p>
+        <form>
+        <fieldset>
+        <ul>
+          <li class= "answerItem">
+            <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+            <span>${STORE[questionNumber].answers[0]}</span>
+            </label>
+          </li>
+          <li class= "answerItem">
+            <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+            <span>${STORE[questionNumber].answers[1]}</span>
+            </label>
+          </li>
+          <li class= "answerItem">
+            <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+            <span>${STORE[questionNumber].answers[2]}</span>
+            </label>
+          </li>
+          <li class= "answerItem">
+            <label class="answerOption">
+            <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+            <span>${STORE[questionNumber].answers[3]}</span>
+            </label>
+          </li>
+        </ul>
+        <button type="submit" class="buttonSubmit">Submit</button>
+        </fieldset>
+        </form>
+        </div>`;
+    } else {
+        renderResults();
+        restartQuiz();
+        $('.questionNumber').text(10)
+    }
+}
+
+//increment question number
+function changeQuestionNumber () {
+      questionNumber ++;
+    
+      $('.questionNumber').text(questionNumber+1);
+  }
+
+//increment score
+function changeScore () {
+    score ++;
+  }
+
+//start quiz, hide start div and show question div
+function startQuiz() {
+    $('.quizStart').on('click', '.buttonStart', function(event) {
+//hide start div
+    $('.quizStart').remove();
+//show question div
+    $('.questionAnswerForm').css('display', 'block');
+//question number starts with 1
+    $('.questionNumber').text(1);
+    });
+}
+
+//render question in DOM
+    function renderQuestion() {
+        $('.questionAnswerForm').html(generateQuestion());
+    }
+
+//user selects answer and gets feedback based on whether or not it is correct
+
+function userSelectAnswer() {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+        if (answer === correctAnswer) {
+          selected.parent().addClass('correct');
+          ifAnswerCorrect();
+        } else {
+          selected.parent().addClass('wrong');
+          ifAnswerWrong();
+        }
+      });
+}
+
+function ifAnswerCorrect () {
+    userAnswerFeedbackCorrect();
+    updateScore();
+  }
+  
+  function ifAnswerWrong () {
+    userAnswerFeedbackWrong();
+  }
+
+//after answering a question, user receives 1 of 2 responses, score is updated
+
+//if answer is correct response
+function userAnswerFeedbackCorrect () {
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    $('.questionAnswerForm').html(`<div class="correctFeedback"><img class="feedbackImg" src="https://i.imgur.com/3aHDcXG.jpg" alt="Chris Trager giving you a thumbs up"/>
+    <h2>You got it right!</h2><button type=button class="buttonNext">Next</button></div>`);
+  }
+
+//if answer is wrong response
+function userAnswerFeedbackWrong () {
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    $('.questionAnswerForm').html(`<div class="wrongFeedback"><h2>You got it wrong!</h2>
+    <img class="feedbackImg" src="https://i.imgur.com/Yp0oEHt.jpg" alt="an angry April Ludgate snarling at you"/>
+    <p>The correct answer is <span>"${correctAnswer}"</span>!</p><button type="submit" button class="buttonNext">Next</button></div>`);
+  }
+
+//update score progress
+function updateScore () {
+    changeScore();
+    $('.score').text(score);
+  }
+//after answering all questions, display HTML for end of quiz results
+// dependent on score (3 possible screens)
+function renderResults () {
+    if (score >= 8) {
+      $('.questionAnswerForm').html(`<div class="results correctFeedback"><h2>Good work, I'm proud of you.</h2>
+      <img src="https://i.imgur.com/K1yKaSl.jpg" alt="Ron Swanson, holding a saw"/><p>You got ${score} / 10</p><p>"Never half ass two things. Whole ass one thing."</p>
+      <button class="buttonReload">Restart Quiz</button></div>`);
+    } else if (score < 8 && score >= 4) {
+      $('.questionAnswerForm').html(`<div class="results correctFeedback"><h2>Almost there!</h2>
+      <img src="https://i.imgur.com/knBRsyK.jpg" alt="Ben Wyatt holding a Cone of Dunshire"/><p>You got ${score} / 10</p><p>"Are the cones a metaphor? Well, yes and no."</p><button class="buttonReload">Restart Quiz</button></div>`);
+    } else {
+      $('.questionAnswerForm').html(`<div class="results correctFeedback"><h2>Dammit, Jerry!</h2><img src="https://i.imgur.com/bLPJTPt.jpg" alt="Jerry, holding a phone and looking scared"/><p>You got ${score} / 10</p><p>"Aw, Jeez..."</p><button class="buttonReload">Restart Quiz</button></div>`);
+    }
+  } 
+
+
+//what happens when user clicks 'next'
+function renderNextQuestion () {
+    $('main').on('click', '.buttonNext', function (event) {
+      changeQuestionNumber();
+      renderQuestion();
+      userSelectAnswer();
+    });
+  }
+//reload quiz and start over
+function restartQuiz () {
+    $('main').on('click', '.buttonReload', function (event) {
+      location.reload();
+    });
+  }
+
+//run quiz functions
+function createQuiz () {
+    startQuiz();
+    renderQuestion();
+    userSelectAnswer();
+    renderNextQuestion();
+  }
+  
+  $(createQuiz);
